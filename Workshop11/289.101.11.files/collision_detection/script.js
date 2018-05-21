71f07b4f1a08f7dc261ccfed28350c016e3655d3
McLuckie <18037140@wsom-072785.massey.ac.nz>
2018-05-21 16:02:50 +1200

    added

--- Workshop11/289.101.11.files/collision_detection/script.js
+++ Workshop11/289.101.11.files/collision_detection/script.js
@@ -15,22 +15,61 @@ Next, add the following features:
 var puck_x = 210;
 var puck_y = 210;
 var puck = document.getElementById('puck');
+var speed = 50;
 
 //get ID
 
 puck.style.top = puck_y + 'px';
 puck.style.left = puck_x + 'px';
+puck.style.updown = puck_y + 'px';
+puck.style.updown = puck_x - 'px';
 
+//funtion to log coordinates
+
+function logCoordinates(){
+console.log('x=' + puck_x + 'y=' + puck_y);    
+}
+function checkCollision(){
+    if(puck_x>250 && puck_x< 350){
+    gap.style.backgroundColor = 'red';
+    
+    console.log('collision!');
+}
+    else{
+        gap.style.backgroundColor = 'white';
+    }
+}
 
 //move left
 document.getElementById('left').addEventListener('click',function(){
-   puck_x-= 50;
-   document.getElementById('puck').style.left= puck_x+ 'px';                                         
+   puck_x-= speed;
+   document.getElementById('puck').style.left= puck_x+ 'px'; 
+   logCoordinates();
+   checkCollision();
 });
 
 //move right
 document.getElementById('right').addEventListener('click',function(){
-   puck_x+= 50;
-   document.getElementById('puck').style.left= puck_x+ 'px';                                    
+   puck_x+= speed;
+   document.getElementById('puck').style.left= puck_x+ 'px'; 
+    logCoordinates();
+    checkCollision();
 });
+
+//move up
+document.getElementById('up').addEventListener('click',function(){
+   puck_y-= speed;
+   puck.style.up = puck_y- 'px'; 
+   logCoordinates();
+   checkCollision();
+});
+
+//move down
+document.getElementById('down').addEventListener('click',function(){
+   puck_y+= speed;
+   puck.style.up = puck_y+ 'px'; 
+   logCoordinates();
+   checkCollision();
+});
+
 
