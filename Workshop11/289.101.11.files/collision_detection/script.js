257fb13dcc5af070447d7bc1a4ee2e48bad37198
McLuckie <18037140@wsom-072785.massey.ac.nz>
2018-05-21 15:13:53 +1200

    Added

--- Workshop11/289.101.11.files/collision_detection/script.js
+++ Workshop11/289.101.11.files/collision_detection/script.js
@@ -9,3 +9,28 @@ Next, add the following features:
 - create an item that makes the puck smaller
 
 */
+
+//declaring puck location
+
+var puck_x = 210;
+var puck_y = 210;
+var puck = document.getElementById('puck');
+
+//get ID
+
+puck.style.top = puck_y + 'px';
+puck.style.left = puck_x + 'px';
+
+
+//move left
+document.getElementById('left').addEventListener('click',function(){
+   puck_x-= 50;
+   document.getElementById('puck').style.left= puck_x+ 'px';                                         
+});
+
+//move right
+document.getElementById('right').addEventListener('click',function(){
+   puck_x+= 50;
+   document.getElementById('puck').style.left= puck_x+ 'px';                                    
+});
+
